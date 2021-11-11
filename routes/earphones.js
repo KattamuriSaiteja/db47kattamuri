beddf78a2beea732ae9c086fee5e745ae1b7b0cd
Kattamuri Saiteja <34226707+Kattamur@users.noreply.github.com>
2021-11-10 21:06:50 -0600

    updated comment

--- routes/earphones.js
+++ routes/earphones.js
@@ -2,7 +2,7 @@ var express = require('express');
 const earphones_controlers = require('../controllers/earphones');
 var router = express.Router();
 
-/* GET costumes */
+/* GET earphones */
 router.get('/', earphones_controlers.earphones_view_all_Page);
 module.exports = router;
 
