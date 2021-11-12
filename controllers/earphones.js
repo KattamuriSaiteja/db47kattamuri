91d1cd73c16beee3cb079c9edd3dd75d76d2e63d
Kattamuri Saiteja <34226707+Kattamur@users.noreply.github.com>
2021-11-11 23:09:27 -0600

    Added 12 lab

--- controllers/earphones.js
+++ controllers/earphones.js
@@ -22,13 +22,15 @@ exports.earphones_view_all_Page = async function (req, res) {
     }
 };
 
-/*
-exports.earphones_list = function (req, res) {
-    res.send('NOT IMPLEMENTED: earphones list');
-};*/
-// for a specific earphones.
-exports.earphones_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: earphones detail: ' + req.params.id);
+exports.earphones_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await earphones.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle earphones create on POST.
 exports.earphones_create_post = async function (req, res) {
@@ -51,6 +53,21 @@ exports.earphones_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: earphones delete DELETE ' + req.params.id);
 };
 // Handle earphones update form on PUT.
-exports.earphones_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: earphones update PUT' + req.params.id);
+exports.earphones_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await earphones.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.brand) toUpdate.brand = req.body.brand;
+        if (req.body.earbudsModel) toUpdate.earbudsModel = req.body.earbudsModel;
+        if (req.body.jackMM) toUpdate.jackMM = req.body.jackMM;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
 };
