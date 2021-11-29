25a4622bf207ea41a494a64ab40e049c4d871f78
Kattamuri Saiteja <34226707+Kattamur@users.noreply.github.com>
2021-11-29 15:20:12 -0600

    Added login and register pug

--- routes/earphones.js
+++ routes/earphones.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const earphones_controlers = require('../controllers/earphones');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET earphones */
 router.get('/', earphones_controlers.earphones_view_all_Page);
@@ -19,7 +28,7 @@ module.exports = router;
 router.get('/create', earphones_controlers.earphones_create_Page);
 
 /* GET create update page */
-router.get('/update', earphones_controlers.earphones_update_Page);
+router.get('/update', secured, earphones_controlers.earphones_update_Page);
 
 /* GET create earphones page */
 router.get('/delete', earphones_controlers.earphones_delete_Page);
